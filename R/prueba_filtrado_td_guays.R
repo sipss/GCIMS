86ac5eabb2cc8119555a4518594e6b6208e65613
Luis-Fernandez-Romero <lfernrom@gmail.com>
2020-09-23 14:45:05 +0200

    fusión de las funciones de filtrado y creación script de prueba
    Ahora se puede usar la misma funcion para filtrar el tiempo de retención y el tiempo de deriva.
    
    El nombre de la función ahora es gcims_smoothing.
    
    Añado una cabecera rudimentaria
    
    Ejemplo no valido (solo es para como se hace)
    
    Añado una script (prueba filtrado_td_guays) para ver que funciona.
    
    Cuidado con que son filas y que son columnas (diría que está cambiado respecto a matlab)

---
+++ R/prueba_filtrado_td_guays.R
@@ -0,0 +1,13 @@
+library (signal)
+library (R.matlab)
+setwd ('C:/Users/Biosignal/Desktop/LCMS_2019/GCIMS/R')
+dir_in <- 'C:/Users/Biosignal/Desktop/Files/HAM/JAMONES/data'
+dir_out <- 'C:/Users/Biosignal/Desktop/Files/HAM/JAMONES/filtd'
+filter_length <- 19
+polynomial_order <- 2
+by_rows <- TRUE
+samples <- c(2)
+
+gcims_smoothing(dir_in, dir_out, filter_length, polynomial_order, by_rows = FALSE, samples)
+
+
