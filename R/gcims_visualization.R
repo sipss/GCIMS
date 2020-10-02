072359115f78519da83d0df98e1f43f3421c9a47
Celia Mallafré Muro <37548531+celiamallafre@users.noreply.github.com>
2020-10-02 18:14:04 +0200

    gcims_visualization
    First funcion for visualization, the colors palete has to be changed

---
+++ R/gcims_visualization.R
@@ -0,0 +1,31 @@
+#' SAmples Visualization
+
+
+gcims_visualization <- function(dir_in, samples){
+
+  print(" ")
+  print("  /////////////////////////////////////")
+  print(" /   Samples Spectra Visualization   /")
+  print("/////////////////////////////////////")
+  print(" ")
+
+  library(ggplot2)
+  library(metR)
+  library(reshape2)
+  library(tidyverse)
+
+  setwd(dir_in)
+  m = 0;
+  for (i in (samples)){
+    m = m + 1
+    print(paste0("Sample ", m, " of ", length(samples)))
+    aux_string <- paste0("M", i, ".rds")
+    aux <- readRDS(aux_string)
+    retentiontime <- c(0:(dim(aux)[1]-1))
+    rownames(aux) <- retentiontime
+    moltaux <- melt(t(aux))
+    colnames(moltaux) <- c("Dirft Time", "Retention Time", "Value")
+    ggplot(moltaux, aes(x = `Dirft Time`, y = `Retention Time`, z = Value)) +
+      geom_contour_fill()
+  }
+}
