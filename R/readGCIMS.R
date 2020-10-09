be3d3e5f67ae448ccdf21b503f67b7088c0cb7ab
Celia Mallafré Muro <37548531+celiamallafre@users.noreply.github.com>
2020-10-09 10:59:51 +0200

    Update readGCIMS.R
    New version of the reading function

--- R/readGCIMS.R
+++ R/readGCIMS.R
@@ -18,15 +18,22 @@
 #' print(dataset_pos)
 #' }
 
-readGCIMS <- function(x) {
+readGCIMS <- function(dir_in, dir_out) {
+  setwd(dir_in)
   metadata <- NULL
-  if (str_contains(x, ".csv")){ ## I ask if my file (x) contains ".csv in the name. It it is a csv, I read the file.
-    file <- read_csv(x, skip = 130)
-    data <- file[-1, -c(1:2)]
-  }else if (str_contains(x, ".mat")) {
-    data <- readMat(x)[[1]]
-  }else { ## If my file i is not a csv, I show the warning
-    warning("This fild is not a .csv or a .mat file")
+  files <- list.files(pattern = ".csv")
+  for (i in 1:length(files)){
+    if (str_contains(files[i], ".csv")){ ## I ask if my file (x) contains ".csv in the name. It it is a csv, I read the file.
+      file <- readr::read_csv(files[i], skip = 130)
+      dd <- file[-1, -c(1:2)]
+      dd <- list(metadata, dd)
+      setwd(dir_out)
+      saveRDS(data, file = paste0("M", i, ".rds"))
+      setwd(dir_in)
+    }else if (sjmisc::str_contains(x, ".mat")) {
+      dd <- R.matlab::readMat(x)[[1]]
+    }else { ## If my file i is not a csv, I show the warning
+      warning("This fild is not a .csv or a .mat file")
+    }
   }
-  data <- list(metadata, data)
 }
