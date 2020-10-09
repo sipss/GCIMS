d6b14ea57b586cb532680100531a7eec85294311
Luis-Fernandez-Romero <lfernrom@gmail.com>
2020-10-09 15:42:15 +0200

    new_function: gcims_plot_chrom
    I merges gcims_plot_eics with gcims_plot_tics
    
    It also includes a retention time range
    
    auto checks for input retention time ranges and dritf time indexes included

--- R/gcims_visualization.R
+++ R/gcims_visualization.R
@@ -54,13 +54,14 @@ gcims_visualization <- function(dir_in, sample_num){
 
 
 
-#' Total Ion Chromatogram visualization
-
+#' Plots TICs or EICs.
 
 #' @param dir_in          The input directory.
 #' @param samples         The set of samples to be
 #'                        to be visualized.
-
+#' @param rt_range        Min a Max retention time values. NULL by
+#'                        default.
+#' @param td_ind          Selected drift time value. NULL by default.
 #' @return A plot of the Total Ion Chromatograms.
 #' @family Visualization functions
 #' @export
@@ -78,20 +79,47 @@ gcims_visualization <- function(dir_in, sample_num){
 #' print(dataset_pos)
 #' }
 
-gcims_plot_tics <- function(dir_in, samples){
+gcims_plot_chrom <- function(dir_in, samples, td_ind = NULL, rt_range = NULL){
 
   Retention_Time <- Index <- Value <- Sample <- NULL
 
   print(" ")
-  print("  //////////////////////")
-  print(" /     Plottig TICs   /")
-  print("//////////////////////")
+  print("  ///////////////////////////////")
+  print(" /     Plottig Chromatograms   /")
+  print("///////////////////////////////")
   print(" ")
+
+  if(is.null(td_ind)){
+    #it's OK if it's null
+  } else if((is.numeric(td_ind)) & (length(td_ind) == 1)){ #MODIFICAR ESTO PARA TIEMPOS NO PARA INDICES (O ANTES)
+    if(td_ind > 0){
+    #numeric, positive and scalar it's: OK
+    } else{
+      stop ("The variable td_ind can't be negative")
+    }
+  } else{
+    stop ("Incorrect input values for td_ind: It must be either NULL or a positive scalar integer")
+  }
 
   setwd(dir_in)
   aux_string <- paste0("M", samples[1], ".rds")
   aux <- readRDS(aux_string)
-  num_of_rows <- dim(aux)[1]
+
+  if(is.null(rt_range)){
+    num_of_rows <- dim(aux)[1]
+    sel_index <- 1:num_of_rows
+  } else if((class(rt_range[1]: rt_range[2]) == "integer") & (rt_range[2] > rt_range[1])){
+    if((rt_range[1] >= 1) & (rt_range[2] <= dim(aux)[1])){ #MODIFICAR ESTO PARA TIEMPOS NO PARA INDICES (O ANTES)
+      num_of_rows <- length(rt_range[1]: rt_range[2])
+      sel_index <- 1:num_of_rows
+      } else {
+        stop("Index out of bounds")
+        }
+  } else {
+        stop("Possible errors: 1) rt_range is not an integer vector, 2) or rt_range[2] <= rt_range[1])")
+
+  }
+
   rm(aux_string, aux)
 
   m <- 0
@@ -101,7 +129,12 @@ gcims_plot_tics <- function(dir_in, samples){
     print(paste0("Sample ", m, " of ", length(samples)))
     aux_string <- paste0("M", i, ".rds")
     aux <- readRDS(aux_string)
-    tics[, m] <- rowSums(aux)
+    if (is.null(td_ind)){
+      tics[, m] <- rowSums(aux[sel_index, ])
+    } else {
+      tics[, m] <- aux[sel_index, td_ind]
+    }
+    #tics[, m] <- rowSums(aux[sel_index, ])
     rm(aux_string, aux)
   }
   rm(m)
@@ -111,6 +144,12 @@ gcims_plot_tics <- function(dir_in, samples){
   moltics <- melt(tics)
   colnames(moltics) <- c( "Retention_Time", "Index", "Value")
 
+  if (is.null(td_ind)){
+    plot_title <- "Total Ion Chromatogram"
+  } else{
+    plot_title <- "Extracted Ion Chromatogram"
+  }
+
   moltics <- moltics %>%
                 mutate (Sample = as.factor(samples[Index]))
 
@@ -119,90 +158,11 @@ gcims_plot_tics <- function(dir_in, samples){
     geom_line() +
     labs(x="Retention Time (a.u.)",
          y="Intensity (a.u.)",
-         color = "Samples",
-         title = "Total Ion Chromatograms") +
+         color = "Sample",
+         title = plot_title) +
     theme_minimal()
   print(p)
 }
-
-
-#' Plot Sample Extracted Ion Chromatrogram
-
-
-#' @param dir_in          The input directory.
-#' @param samples         The number corresponding to the sample
-#'                        to be visualized.
-#' @param td_ind          Drift time Index (to be modified in future)
-#' @return An extracted ion chromatogram
-
-#' @return An Extracted Ion Chromatogram,
-#' @family Visualization functions
-#' @export
-#' @importFrom dplyr mutate
-#' @importFrom magrittr '%>%'
-#' @importFrom reshape2 melt
-#' @importFrom ggplot2 ggplot aes labs theme_minimal geom_line
-#' @examples
-#' \dontrun{
-#' dataset_2_polarities <- lcms_dataset_load(system.file("extdata",
-#'                                                      "dataset_metadata.rds",
-#'                                                      package = "NIHSlcms"))
-#' dataset_pos <- lcms_filter_polarity(dataset_2_polarities, polarity. = 1)
-#'
-#' print(dataset_pos)
-#' }
-
-gcims_plot_eics <- function(dir_in, samples, td_ind){
-
-  Retention_Time <- Index <- Value <- Sample <- NULL
-
-  print(" ")
-  print("  //////////////////////")
-  print(" /     Plottig EICs   /")
-  print("//////////////////////")
-  print(" ")
-
-  setwd(dir_in)
-  aux_string <- paste0("M", samples[1], ".rds")
-  aux <- readRDS(aux_string)
-  num_of_rows <- dim(aux)[1]
-  rm(aux_string, aux)
-
-  m <- 0
-  eics <- matrix(0, nrow = num_of_rows, ncol = length(samples))
-  print(dim(eics))
-  for (i in samples){
-    m <- m + 1
-    print(paste0("Sample ", m, " of ", length(samples)))
-    aux_string <- paste0("M", i, ".rds")
-    aux <- readRDS(aux_string)
-    print(dim(aux))
-    eics[, m] <- aux[, td_ind]
-    rm(aux_string, aux)
-  }
-  rm(m)
-
-  retentiontime <- c(0:(dim(eics)[1]-1))
-  rownames(eics) <- retentiontime
-  molteics <- melt(eics)
-  colnames(molteics) <- c( "Retention_Time", "Index", "Value")
-
-  molteics <- molteics %>%
-    mutate (Sample = as.factor(samples[Index]))
-
-
-  p <- ggplot(molteics, aes(x = Retention_Time, y = Value, color = Sample)) +
-    geom_line() +
-    labs(x="Retention Time (a.u.)",
-         y="Intensity (a.u.)",
-         color = "Samples",
-         title = "Extracted Ion Chromatograms") +
-    theme_minimal()
-  print(p)
-}
-
-
-
 
 
 #' Total Ion Spectrum visualization
