baaf5be3c12c3c624b8fefc81737abc547daf8eb
Luis-Fernandez-Romero <lfernrom@gmail.com>
2020-10-15 00:15:12 +0200

    Update gcims_visualization.R
    Now it plots retention and drift time ticks in the axes

--- R/gcims_visualization.R
+++ R/gcims_visualization.R
@@ -43,59 +43,119 @@ gcims_view_sample <- function(dir_in, sample_num, rt_range = NULL, dt_range = NU
   aux <- t(as.matrix(aux_list$data$data_df)) #new
 
 #SOME CHECKS
-  if(is.null(rt_range)){
-  } else if((class(rt_range[1]: rt_range[2]) == "integer") & (rt_range[2] > rt_range[1])){
-    if((rt_range[1] >= 1) & (rt_range[2] <= dim(aux)[1])){ #MODIFICAR ESTO PARA TIEMPOS NO PARA INDICES (O ANTES)
-    } else {
-      stop("Index out of bounds")
-    }
-  } else {
-    stop("Possible errors: 1) rt_range is not an integer vector, 2) or rt_range[2] <= rt_range[1])")
+  retention_time <- aux_list$data$retention_time
+  drift_time <- aux_list$data$drift_time
+
+  if(is.null(rt_range)){# old
+    rt_ind <- c(1, dim(aux)[1]) #New
+
+  } else{
+    rt_ind  <- c(which.min(abs(retention_time - rt_range[1])), which.min(abs(retention_time - rt_range[2]))) #New
   }
 
-  if(is.null(dt_range)){
-  } else if((class(dt_range[1]: dt_range[2]) == "integer") & (dt_range[2] > dt_range[1])){
-    if((dt_range[1] >= 1) & (dt_range[2] <= dim(aux)[2])){ #MODIFICAR ESTO PARA TIEMPOS NO PARA INDICES (O ANTES)
-    } else {
-      stop("Index out of bounds")
-    }
-  } else {
-    stop("Possible errors: 1) dt_range is not an integer vector, 2) or dt_range[2] <= dt_range[1])")
+
+
+  if(is.null(dt_range)){# old
+    dt_ind <- c(1, dim(aux)[2]) #New
+  } else{
+    dt_ind  <- c(which.min(abs(drift_time - dt_range[1])), which.min(abs(drift_time - dt_range[2]))) #New
   }
 
-  #END CHECKS
+  sel_index_rt <- rt_ind[1]: rt_ind[2]#old
+  sel_index_dt <- dt_ind[1]: dt_ind[2]#old
 
-  if (is.null(rt_range)){#New
-    sel_index_rt <- 1:dim(aux)[1]#New
-    retentiontime <- c(0:(dim(aux)[1] - 1))#New
-  } else{#New
-    sel_index_rt <- rt_range[1]: rt_range[2]#New
-    retentiontime <- c((rt_range[1] - 1):(rt_range[2] - 1))#New (OJOOOO: que despues seran tiempos, no indices)
-  }#New
+  retention_time <- retention_time[sel_index_rt]
+  drift_time <- drift_time[sel_index_dt]
 
-
-  if (is.null(dt_range)){#New
-    sel_index_dt <- 1:dim(aux)[2]#New
-  } else{#New
-    sel_index_dt <- dt_range[1]: dt_range[2]#New
-  }#New
-
-  aux <- aux[sel_index_rt, sel_index_dt]#New
-  rownames(aux) <- retentiontime #New
+  aux <- aux[sel_index_rt, sel_index_dt]#old
+  rownames(aux) <- retention_time #old
 
   moltaux <- melt(t(aux))
   colnames(moltaux) <- c("Drift_Time", "Retention_Time", "Value")
 
+
+#We do this in order to plot the data using geom_raster that is faster than geom_tile
+#perhaps a previous interpolation is needed to avoid this patch:
+  rep_dt_index <- rep(seq(from = 1, to = dim(aux)[2], by = 1), times = dim(aux)[1])
+  #rep_dt_index <- rep(sel_index_dt, times = dim(aux)[1])
+  drift_time_period <- mean(diff(drift_time))
+  #corr_drift_time <- seq(from = sel_index_dt[1], by = drift_time_period, length.out = length(drift_time))
+  corr_drift_time <- seq(from = drift_time[1], by = drift_time_period, length.out = length(drift_time))
+  moltaux$Drift_Time <- corr_drift_time[rep_dt_index]
+
   rm(aux, aux_string)
   p <- ggplot(moltaux, aes(x = Drift_Time, y = Retention_Time, fill = Value)) +
-    geom_raster() +
+    geom_raster(interpolate = FALSE) +
     scale_fill_viridis(discrete = FALSE, option = "A", direction = -1) +
-    labs(x="Drift Time (a.u.)",
-         y="Retention Time (a.u.)",
+    labs(x="Drift Time (ms)",
+         y="Retention Time (s)",
          title = "Sample Matrix Image",
          fill = "Intensity") +
     theme_minimal()
   print(p)
+
+
+  # else if((class(rt_ind[1]: rt_ind[2]) == "integer") & (rt_ind[2] > rt_ind[1])){
+  #   #else if((class(rt_range[1]: rt_range[2]) == "integer") & (rt_range[2] > rt_range[1])){ # old
+  #   if((rt_ind[1] >= 1) & (rt_ind[2] <= dim(aux)[1])){ # NEw
+  #   #if((rt_range[1] >= 1) & (rt_range[2] <= dim(aux)[1])){ OLD
+  #   } else {
+  #     stop("Index out of bounds")
+  #   }
+  # } else {
+  #   stop("Possible errors: 1) rt_range is not a valid (numeric) vector, 2) or rt_range[2] <= rt_range[1])")
+  # }
+  #
+  # if(is.null(dt_range)){ #old
+  # } else if((class(dt_ind[1]: dt_ind[2]) == "integer") & (dt_ind[2] > dt_ind[1])){
+  #   #else if((class(dt_range[1]: dt_range[2]) == "integer") & (dt_range[2] > dt_range[1])){ #OLD
+  #   if((dt_ind[1] >= 1) & (dt_ind[2] <= dim(aux)[2])){ #MODIFICAR ESTO PARA TIEMPOS NO PARA INDICES (O ANTES)
+  #   #if((dt_range[1] >= 1) & (dt_range[2] <= dim(aux)[2])){ OLD
+  #   } else {
+  #     stop("Index out of bounds")
+  #   }
+  # } else {
+  #   stop("Possible errors: 1) dt_range is not a valid (numeric) vector, 2) or dt_range[2] <= dt_range[1])")
+  # }
+
+  #END CHECKS
+
+  # if (is.null(rt_range)){#old
+  #   sel_index_rt <- 1:dim(aux)[1]#old
+  #   #retention_time <- c(0:(dim(aux)[1] - 1))#old
+  # } else{#New
+  #   sel_index_rt <- rt_ind[1]: rt_ind[2]
+  #   #sel_index_rt <- rt_range[1]: rt_range[2]#old
+  #   #retention_time <- c((rt_range[1] - 1):(rt_range[2] - 1)) # old#New (OJOOOO: que despues seran tiempos, no indices)
+  #   retention_time <- retention_time[sel_index_rt]   #New
+  # }#New
+  #
+  #
+  # if (is.null(dt_range)){#old
+  #   sel_index_dt <- 1:dim(aux)[2]#old
+  # } else{#New
+  #   sel_index_dt <- dt_ind[1]: dt_ind[2] #New
+  #   drift_time <- drift_time[sel_index_dt] #New
+  #   #New
+  #   #sel_index_dt <- dt_range[1]: dt_range[2]#old
+  # }#New
+
+  # aux <- aux[sel_index_rt, sel_index_dt]#old
+  # rownames(aux) <- retention_time #old
+  #
+  # moltaux <- melt(t(aux))
+  # colnames(moltaux) <- c("Drift_Time", "Retention_Time", "Value")
+  #
+  # rm(aux, aux_string)
+  # p <- ggplot(moltaux, aes(x = Drift_Time, y = Retention_Time, fill = Value)) +
+  #   geom_raster() +
+  #   scale_fill_viridis(discrete = FALSE, option = "A", direction = -1) +
+  #   labs(x="Drift Time (ms)",
+  #        y="Retention Time (s)",
+  #        title = "Sample Matrix Image",
+  #        fill = "Intensity") +
+  #   theme_minimal()
+  # print(p)
 }
 
 
