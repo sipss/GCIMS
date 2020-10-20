303ffc8cede5feaa4512dc2e6b1e77f578c14140
Luis-Fernandez-Romero <lfernrom@gmail.com>
2020-10-20 18:13:16 +0200

    Function to interpolate data
    To be enhanced

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -37,3 +37,80 @@ gcims_unfold <- function(dir_in, dir_out, samples){
     setwd(dir_in)
   }
 }
+
+#' Interpolates data according to retention and drift time sampling frequencies
+
+
+#' @param dir_in          The input directory.
+#' @param dir_out         The output directory.
+#' @param samples         The set of samples to be processed.
+#' @param by_rows         Logical. Direction to apply the function. If TRUE it
+#'                        is applied by rows (drift time direction).
+#'                        If FALSE, applied by columns
+#'                        (that is the retention time direction).
+#'
+#' @return An interpolated dataset
+#' @family Utility functions
+#' @export
+#' @importFrom signal interp1
+#' @examples
+#' \dontrun{
+#' dataset_2_polarities <- lcms_dataset_load(system.file("extdata",
+#'                                                      "dataset_metadata.rds",
+#'                                                      package = "NIHSlcms"))
+#' dataset_pos <- lcms_filter_polarity(dataset_2_polarities, polarity. = 1)
+#'
+#' print(dataset_pos)
+#' }
+
+gcims_interpolate <- function(dir_in, dir_out, samples, by_rows){
+  print(" ")
+  print("  /////////////////////////")
+  print(" /   Filtering the data /")
+  print("/////////////////////////")
+  print(" ")
+
+  setwd(dir_in)
+  m = 0
+  for (i in samples){
+    m = m + 1
+    print(paste0("Sample ", m, " of ", length(samples)))
+    aux_string <- paste0("M", i, ".rds")
+    aux_list <- readRDS(aux_string) #new
+    aux <- as.matrix(aux_list$data$data_df)
+
+    if (by_rows == TRUE){
+      aux <- t(aux)
+      x <- aux_list$data$drift_time
+      xi <- seq(from = x[1],
+                by = mean(diff(x)),
+                length.out = length(x))
+    } else if (by_rows == FALSE){
+      x <- aux_list$data$retention_time
+      xi <- seq(from = x[1],
+                by = mean(diff(x)),
+                length.out = length(x))
+    }
+
+    n <- dim(aux)[1]
+
+    for (j in (1:n)){
+      aux[j, ] <- interp1(x, aux[j, ], xi, method = "linear")
+    }
+
+    if (by_rows == TRUE){
+      aux <- t(aux)
+      aux_list$data$drift_time <- xi
+    } else if (by_rows == FALSE){
+      aux_list$data$retention_time <- xi
+    }
+
+    aux_list$data$data_df <- aux
+    M <- aux_list
+    setwd(dir_out)
+    saveRDS(M, file = paste0("M", i, ".rds"))
+    setwd(dir_in)
+  }
+
+
+}
