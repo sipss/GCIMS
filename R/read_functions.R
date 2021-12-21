86f318583a643fb457e81810f3ecf95fa7a737d1
Sergio Oller <sergioller@gmail.com>
2021-12-21 13:27:02 +0100

    Update reat_mat function

--- R/read_functions.R
+++ R/read_functions.R
@@ -226,45 +226,36 @@ read_mea <- function(filename) {
 #'
 #' @param dir_in          The input directory.
 #' @param dir_out         The output directory.
-#' @return An R object that contains the matrix and the retention times
+#' @return NULL
 #' @family Reading functions
 #' @export
-#' @importFrom R.matlab readMat
 #' @examples
 #' \dontrun{
-#' dataset_2_polarities <- lcms_dataset_load(system.file("extdata",
-#'                                                      "dataset_metadata.rds",
-#'                                                      package = "NIHSlcms"))
-#' dataset_pos <- lcms_filter_polarity(dataset_2_polarities, polarity. = 1)
-#'
-#' print(dataset_pos)
+#' gcims_read_mat("/path/to/dir/with/mat/files", "where_to_save_samples_in_rds/")
 #' }
 
 gcims_read_mat <- function(dir_in, dir_out) {
-  setwd(dir_in)
-
   print(" ")
   print("  /////////////////////////")
   print(" /    Reading Samples    /")
   print("/////////////////////////")
   print(" ")
 
-  metadata <- NULL
-  files <- list.files(pattern = ".mat")
+  files <- list.files(path = dir_in, pattern = ".mat$")
   if (length(files) == 0){
     stop("This folder does not contains any .mat file")
   }
-  m <- 0
-  for (i in 1:length(files)){
-    m <- m + 1
-    print(paste0("Sample ", m, " of ", length(files)))
-    aux_string <- paste0("M", i, ".rds")
-    metadata$Name <- i
-    dd <- readMat(files[i])[[1]]
-    dd <- list(metadata = metadata, data = dd)
-    setwd(dir_out)
-    saveRDS(dd, file = paste0("M", i, ".rds"))
-    setwd(dir_in)
+  for (i in seq_along(files)) {
+    print(paste0("Sample ", i, " of ", length(files)))
+    dd <- R.matlab::readMat(file.path(dir_in, files[i]))[[1]]
+    dd <- list(
+      metadata = list(Name = i),
+      data = dd
+    )
+    if (!dir.exists(dir_out)) {
+      dir.create(dir_out)
+    }
+    saveRDS(dd, file = file.path(dir_out, paste0("M", i, ".rds")))
   }
 }
 
