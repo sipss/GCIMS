e675a71639e5bfdd61dbac79e294f257f432917c
Sergio Oller <sergioller@gmail.com>
2021-12-31 11:50:50 +0100

    Do a very basic test on the read_mea output to ensure the test actually runs

--- tests/testthat/test-read_mea.R
+++ tests/testthat/test-read_mea.R
@@ -1,4 +1,5 @@
 test_that("read_mea does not crash", {
   mea_file <- system.file("extdata/sample_formats/small.mea.gz", package = "GCIMS")
   sample <- read_mea(mea_file)
+  expect_s4_class(sample, "GCIMSSample")
 })
