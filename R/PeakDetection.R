05cf61121ff114e12b348225270b6fa66aaa0ffa
Celia Mallafré Muro <celiamallafre@gmail.com>
2022-06-30 18:33:41 +0200

    PeakDetectionFinal
    Final function with all the improvements and all the internal functions

--- R/PeakDetection.R
+++ R/PeakDetection.R
@@ -37,7 +37,7 @@
 #' files <- list.files(path = dir_out, pattern = ".rds", all.files = FALSE, full.names = TRUE)
 #' invisible(file.remove(files))
 #' setwd(current_dir)
-gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
+gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level){
   print(" ")
   print("  //////////////////////////")
   print(" /    Selecting the ROIs  /")
@@ -79,8 +79,8 @@ gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
     # quantile(daux)
     # quantile(daux, c(0.25, 0.75))
 
-    patch <- daux[100:200,2000:2100]
-    sigmaNoise <- stats::sd(patch)
+    patch <- daux[4100:4200, 100:200]
+    sigmaNoise <- sd(patch)
 
     # tt <- aux < quantile(aux, 0.15)
     # indx_noise <- which(tt == TRUE, arr.ind = TRUE)
@@ -110,11 +110,12 @@ gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
     # For each IMS Spectra:
     for (j in seq_len(num_ims_spectra)) {
       # Find the max (peaks)
-      #locs <- findpeaks(daux[,j], minpeakheight  = noise_level*sigmaNoise,'WidthReference','halfheight', minpeakdistance  = 4*f.c1*fs)
-      locs <- findpeaks(ddt[,j], minpeakheight = noise_level*sigmaNoise, minpeakdistance = 4*sqrt(2)*sigma0)[ ,2]
+      #locs <- pracma::findpeaks(daux[,j], minpeakheight = noise_level*sigmaNoise, minpeakdistance = 4*sqrt(2)*sigma0)[ ,2]
+      locs <- pracma::findpeaks(ddt[,j], minpeakheight = noise_level*sigmaNoise, minpeakdistance = 4*sqrt(2)*sigma0)[ ,2]
 
       # Find the zero-crossing points
       posrt <- findZeroCrossings(ddt[,j])
+      #posrt <- findZeroCrossings(daux[,j])
       tmp <- NULL
       locs_tmp <- NULL
       for (k in seq_along(locs)){
@@ -132,16 +133,20 @@ gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
     }
 
     ## 5.b. Peaks and Zero-Crossing for Drift Time
-    peaksdt <- vector(mode = "list", length = dim(daux)[1]) # Initialization of vector for peaks
-    zeros_dt <- vector(mode = "list", length = dim(daux)[1]) # Initialization of vector for zero crossings
+
+    num_ims_chr <- nrow(daux)
+    peaksdt <- vector(mode = "list", length = num_ims_chr) # Initialization of vector for peaks
+    zeros_dt <- vector(mode = "list", length = num_ims_chr) # Initialization of vector for zero crossings
 
     # For loop that iterates through all the columns
-    for(j in (1:dim(daux)[1])){
+    for(j in seq_len(num_ims_chr)){
       # Find the max (peaks)
-      locs <- findpeaks(t(drt)[j, ], minpeakheight = noise_level*sigmaNoise)[ ,2]
+      locs <- findpeaksRois(t(drt)[j, ], MinPeakHeight = noise_level*sigmaNoise, MinPeakDistance = 1)$loc
+      #locs <- pracma::findpeaks(daux[j, ], minpeakheight = noise_level*sigmaNoise)[ ,2]
 
       # Find the zero-crossing points
-      posdt <- findZeroCrossings(t(drt)[j,])
+      posdt <- findZeroCrossings(t(drt)[j, ])
+      #posdt <- findZeroCrossings(daux[j, ])
       tmp <- NULL
       locs_tmp <- NULL
       for (k in seq_along(locs)){
@@ -158,6 +163,8 @@ gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
       peaksdt[[j]] <- locs_tmp
     }
 
+    length(peaksdt) <- num_ims_chr
+    length(zeros_dt) <- num_ims_chr
 
     # Compute intersection
 
@@ -170,52 +177,79 @@ gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
         c <- peaksdt[[col]]
         zeros_c <- zeros_dt[[col]]
 
-        if(length(intersect(row,c)) >= 1 & (col > rip_start_index)) {
+        if(length(intersect(row,c)) >= 1 & (col > rip_end_index)) {
           peaks <- rbind(peaks, c(row, col))
           minY <- zeros_c[1, c == row]
           maxY <- zeros_c[2, c == row]
 
-          minX <- zeros_columns[columns == col][1]
-          maxX <- zeros_columns[columns == col][2]
+          minX <- zeros_columns[1, columns == col]
+          maxX <- zeros_columns[2, columns == col]
 
           width <- abs(maxX[1] - minX[1])
           height <- abs(maxY[1] - minY[1])
 
           if (minX[1] != maxX[1] & minY[1] != maxY[1] & minX[1] < maxX[1] & minY[1] < maxY[1]){
-            ROIs <- rbind(ROIs, c(minX[1], maxX[1], minY[1], maxY[1]))
+            begindt <- minX[1] - round(0.5*width)
+            beginrt <- minY[1] - round(0.5*height)
+            endt <- maxX[1] + round(0.5*width)
+            endrt <- maxY[1] + round(0.5*height)
+            minX <- begindt*(begindt > 0) + 1
+            maxX <- endt*(endt <= dim(aux)[1]) + (dim(aux)[1] *(endt > dim(aux)[1]))
+            minY <- beginrt*(beginrt > 0) + 1
+            maxY <- endrt*(endrt <= dim(aux)[2]) + (dim(aux)[1] *(endrt > dim(aux)[2]) )
+            ROIs <- rbind(ROIs, c(minX, maxX, minY, maxY))
+            #ROIs <- rbind(ROIs, c(minX[1], maxX[1], minY[1], maxY[1]))
           }
         }
       }
     }
 
 
-    # Merging algorithm
+    for (n in c(1:dim(ROIs)[1])){
+      if (ROIs[n, 2] - ROIs[n, 1] < 0){
+        ROIs[n, ] <- NA
+      }
+      if (ROIs[n, 3] - ROIs[n, 3] < 0){
+        ROIs[n, ] <- NA
+      }
+    }
+
+    ROIs <- na.omit(ROIs)
+    # Merging algorith
 
     thrOverlap <- 0.2
-    aff <- c(1:dim(ROIs)[1])
+    if (dim(ROIs)[1] >= 1) {
+      aff <- c(1:dim(ROIs)[1])
 
-    done <- NULL
-    for (j in (1:dim(ROIs)[1])){
-      done <- c(done, j)
-      R1 <- ROIs[j, ]
-      for (k in c((1:dim(ROIs)[1])[- done])){
-        R2 <- ROIs[k, ]
-        if (aff[k] != j){
-          if (overlapPercentage(R1,R2) > thrOverlap){
-            aff[aff == k] <- j
+      done <- NULL
+      for (j in (1:dim(ROIs)[1])){
+        done <- c(done, j)
+        R1 <- ROIs[j, ]
+        for (k in c((1:dim(ROIs)[1])[- done])){
+          R2 <- ROIs[k, ]
+          if (aff[k] != j){
+            if (abs(overlapPercentage(R1,R2)) > thrOverlap){
+              aff[aff == k] <- j
+            }
           }
         }
       }
     }
 
 
+    colnames(ROIs) <- c("minDT", "maxDT", "minRT", "maxRT")
     ROIs_overlap <- NULL
     peaks_overlap <- NULL
-    uniqueness <- NULL
     labels <- unique(aff)
     AsF <- NULL
     volume <- NULL
+    area <- NULL
+    rtmcs <- NULL
+    dtmcs <- NULL
     saturation <- rep(0, length(labels))
+
+
+
 
     # Search saturation regions
     rip_chrom <- rowSums(aux[rip_start_index: rip_end_index, ]) / length(rip_start_index: rip_end_index)
@@ -224,15 +258,14 @@ gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
 
     for (n in seq_along(labels)){
       idx <- which(aff == labels[n])
-      uniqueness <- c(uniqueness, length(idx))
       R1 <- ROIs[idx[1], ]
       if (length(idx) > 1) {
         for (m in (2:length(idx))){
           R2 <- ROIs[idx[m], ]
-          R1 <- c(min(R1[1], R2[1]), max(R1[2], R2[2]), min(R1[3], R2[3]), max(R1[4], R2[4]))
+          R1 <- c(min(R1[1], R2[1]), max(R1[2], R2[2]),
+                  min(R1[3], R2[3]), max(R1[4], R2[4]))
         }
       }
-
       ROIs_overlap <- rbind(ROIs_overlap, R1)
 
       patch <- aux[R1[1]:R1[2], R1[3]:R1[4]]
@@ -247,24 +280,36 @@ gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
       len_rt <- ROIs_overlap[n, 4] - ROIs_overlap[n, 3]
 
       # roi area
-      area <- len_rt * len_dt
+      area_roi <- len_rt * len_dt
+      area <- c(area, area_roi)
 
       # roi volume
       volume <- c(volume, sum(aux[R1[1]:R1[2], R1[3]:R1[4]]))
 
       # roi center of mass
+      ind <- arrayInd(which.max(aux[R1[1]:R1[2], R1[3]:R1[4]]),
+                      dim(aux[R1[1]:R1[2], R1[3]:R1[4]]))
+      x_cm <- R1[1] + ind[1,1] - 1L
+      y_cm <- R1[3] + ind[1,2] - 1L
       x_cm <- round(compute_integral2(aux[R1[1]:R1[2], R1[3]:R1[4]] * (ROIs_overlap[n, 2] - ROIs_overlap[n, 1])) / volume[n])
       y_cm <- round(compute_integral2(aux[R1[1]:R1[2], R1[3]:R1[4]] * (ROIs_overlap[n, 4] - ROIs_overlap[n, 3])) / volume[n])
-      dt_mc <- ROIs_overlap[n, 1] + x_cm - 1 #min_dt
-      rt_mc <- ROIs_overlap[n, 3] + y_cm - 1 #min_rt
+      # dt_mc <- ROIs_overlap[n, 1] + x_cm - 1 #min_dt
+      # rt_mc <- ROIs_overlap[n, 3] + y_cm - 1 #min_rt
+      # rtmaxs <- c(rtmaxs, y_cm)
+      # dtmaxs <- c(dtmaxs, x_cm)
+
+      v <- rowSums(aux[R1[1]:R1[2], R1[3]:R1[4]])
+      dt_cm1 <- (sum(v * seq_along(v)) / sum(v)) + R1[1]  - 1L
+      v <- colSums(aux[R1[1]:R1[2], R1[3]:R1[4]])
+      rt_cm1 <- (sum(v * seq_along(v)) / sum(v)) + R1[3] - 1L
+      rtmcs <- c(rtmcs, round(rt_cm1))
+      dtmcs <- c(dtmcs, round(dt_cm1))
 
 
       # roi asymmetries
       half_down_area  <- length(1:y_cm) * len_rt
       half_up_area    <- length(y_cm:len_dt) * len_rt
-      half_left_area  <- len_dt * length(1:x_cm)
-      half_right_area <- len_dt * length(x_cm:len_rt)
-      asymetry <- round(((half_down_area - half_up_area) / (area)), 2)
+      asymetry <- round(((half_down_area - half_up_area) / (area_roi)), 2)
       AsF <- c(AsF, asymetry)
 
       saturation_regions <- which(rip_chrom <= saturation_threshold)
@@ -284,8 +329,8 @@ gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
         # No saturation. Do nothing
       } else {
         for (l in (1:dim(saturation_minima)[1])){
-          if ((saturation_minima[l, 1] < rt_mc)
-              & (saturation_minima[l, 2] > rt_mc)) {
+          if ((saturation_minima[l, 1] < y_cm)
+              & (saturation_minima[l, 2] > y_cm)) {
             saturation[n] <- 1
             break
           }
@@ -293,18 +338,22 @@ gcims_rois_selection <- function(dir_in, dir_out, samples, noise_level=3){
       }
     }
 
-    colnames(ROIs_overlap) <- c("minRT", "maxRT", "minDT", "maxDT")
+    colnames(ROIs_overlap) <- c("minDT", "maxDT", "minRT", "maxRT")
 
     aux_list$data$data_df <- round(aux)
     aux_list$data$ROIs <- ROIs_overlap
     aux_list$data$Peaks <- peaks_overlap
-    aux_list$data$Parameters <- rbind(AsF, saturation, uniqueness, volume)
-    M <- aux_list
+    aux_list$data$Parameters <- rbind(AsF, saturation, volume)
+    peaktable <- cbind((1:dim(ROIs_overlap)[1]), dtmcs, rtmcs, ROIs_overlap, area, volume, AsF, saturation)
+    colnames(peaktable) <- c("ID", "ApexDT", "ApexRT", "minDT", "maxDT", "minRT", "maxRT", "Area", "Volume", "AsF", "Saturation")
     setwd(dir_out)
+    write.csv(peaktable, file = paste0("PeakTable", i, ".csv"))
+    M <- aux_list
     saveRDS(M, file = paste0("M", i, ".rds"))
     setwd(dir_in)
   }
 }
+
 
 #' Detect peaks
 #' @noRd
@@ -541,6 +590,7 @@ detect_peaks <- function(intmat, noise_level = 3, area_overlap_frac = 0.2, rip_s
 
 
 
+
 #---------------#
 #   FUNCTIONS   #
 #---------------#
@@ -686,12 +736,12 @@ findZeroCrossings <- function(x){
 #----------------------#
 
 overlapPercentage <- function(ROI1, ROI2){
-  x_left <- max(ROI1[3], ROI2[3])
-  y_top <- max(ROI1[1], ROI2[1])
-  x_right <- min(ROI1[4], ROI2[4])
-  y_bottom <- min(ROI1[2], ROI2[2])
+  x_left <- max(ROI1[1], ROI2[1])
+  y_top <- min(ROI1[4], ROI2[4])
+  x_right <- min(ROI1[2], ROI2[2])
+  y_bottom <- max(ROI1[3], ROI2[3])
 
-  if (!(x_right <= x_left | y_bottom <= y_top)){
+  if (x_right >= x_left | y_top >= y_bottom){
     area1 <- (ROI1[2] - ROI1[1])*(ROI1[4] - ROI1[3])
     area2 <- (ROI2[2] - ROI2[1])*(ROI2[4] - ROI2[3])
     overlapping_area <- (x_right - x_left)*(y_bottom - y_top)
@@ -734,6 +784,85 @@ compute_integral2 <- function(data){
   }
   return(I)
 }
+
+
+
+#------------------#
+#   findpeaksRois  #
+#------------------#
+
+findpeaksRois <- function (data, MinPeakDistance = 1, MinPeakHeight = 1) {
+  ld <- length(data)
+  tmp <- data
+
+  df1 <- diff(data, differences = 1)[c(1, 1:(ld - 1))]
+  df2 <- diff(data, differences = 2)[c(1, 1, 1:(ld - 2))]
+  idx <- which(df1 * c(df1[2:length(df1)], 0) <= 0 & c(df2[2:length(df2)], 0) < 0)
+  max_ind <- which(data[idx] > MinPeakHeight)
+  idx <- idx[max_ind]
+  if (length(idx) > 0) {
+    tmp <- sort(data[idx], decreasing = TRUE, index = TRUE)
+    idx_s <- idx[tmp$ix]
+    D <- with(expand.grid(A = idx_s, B = t(idx_s)), abs(A - B))
+    dim(D) <- c(length(idx_s), length(idx_s))
+    if (dim(D)[1] > 1){
+      diag(D) <- NA
+    }
+    if (any(D) < MinPeakDistance) {
+      i <- 1
+      pointsmax <- seq_along(idx_s)
+      checked <- NULL
+      idx_pruned <- idx_s
+      while (length(pointsmax) > 0) {
+        d <- D[pointsmax[1], ]
+        checked <- c(checked, pointsmax[1])
+        pointsmax <- pointsmax[-1]
+        remains <- setdiff(which(d < MinPeakDistance), checked)
+        if (length(remains) > 0) {
+          idx_pruned <- setdiff(idx_pruned, idx_s[remains])
+          checked <- c(checked, remains)
+          pointsmax <- setdiff(pointsmax, checked)
+        }
+      }
+      idx <- idx_pruned
+    }
+
+    idx <- sort(idx)
+    idx.pruned <- idx
+    n <- length(idx)
+    for (i in 1:n) {
+      ind <- round(max(idx[i] - MinPeakDistance/2, 1)):round(min(idx[i] + MinPeakDistance/2, ld))
+      pp <- rep(0L, 3)
+      H <- data[idx[i]]
+      xm <- idx[i]
+      pp <- rep(1L, 3)
+      pp[1] <- pracma::mldivide((ind - xm)^2, (data[ind] - H))
+      pp[2] <- -2 * pp[1] * xm
+      pp[3] <- H + pp[1] * xm^2
+      sigma <- sqrt(abs(1/(2*pp[3])))
+      if (abs(pp[1]) > 0){
+        width <- exp(abs(pp[1]) + abs(pp[2])*sigma^2^2/(2*sigma^2))
+      } else {
+        width <- exp(1 + 1*sigma^2^2/(2*sigma^2))
+      }
+      MinPeakWidth <- (H/2 - 15)
+      MaxPeakWidth <- (H/2 + 20)
+      if ((width > MaxPeakWidth || width < MinPeakWidth) ||
+          pp[1] > 0 || H < MinPeakHeight || data[idx[i]] < H ||
+          abs(idx[i] - xm) > MinPeakDistance/2) {
+        idx.pruned <- setdiff(idx.pruned, idx[i])
+      }
+    }
+
+    idx <- idx.pruned
+    pks <- data[idx]
+    list(pks = pks, loc = idx)
+  } else {
+    list(pks = NULL, loc = NULL)
+  }
+}
+
+
 
 
 gcims_view_ROIs <- function(dir_in, sample_num, rt_range = NULL, dt_range = NULL){
@@ -861,4 +990,3 @@ gcims_view_ROIs <- function(dir_in, sample_num, rt_range = NULL, dt_range = NULL
 
   print(p)
 }
-
