4ab01cf655e15e1a578687c5c722ef6ba8afb0ef
Luis-Fernandez-Romero <lfernandez@ibecbarcelona.eu>
2022-07-19 14:42:37 +0200

    align_td: MODIFIED
    Now the function performs a global multiplicative correction of spectra in one sample. and for all samples

--- R/alignment_functions_NEW.R
+++ R/alignment_functions_NEW.R
@@ -2,8 +2,8 @@
 #'
 #' This spectra alignment function computes the largest peak from
 #' each spectrum, assuming it corresponds to the Reactant Ion Peak.
-#' Then it computes the median RIP position of all the samples and
-#' corrects the drift time for each sample.
+#' Then it computes the RIP position of all the samples and
+#' corrects the all the spectra, for each sample.
 #' The correction is multiplicative, meaning:
 #' drift_time_corr = Kcorr * drift_time
 #'
@@ -19,22 +19,17 @@
 #' @param samples            Numeric vector of integers. Identifies the set of
 #'                           sample to be visualized from the dataset.
 #' @export
-#' @importFrom stats median
-#' @importFrom signal interp1
-#' @importFrom pracma findpeaks
-#' @export
 #'
 align_td <- function(dir_in, dir_out,samples) {
   aux_string <- paste0("M0.rds")
   aux_list <- readRDS(file.path(dir_in, aux_string))
   aux <- as.matrix(aux_list$data$data_df)
   drift_time <- aux_list$data$drift_time
-  rip_position <- apply(aux, MARGIN = 2, which.max)
+  #rip_position <- apply(aux, MARGIN = 2, which.max)
 
-
-  dt_rip_position <- drift_time[rip_position]
-  drift_time_rip_ref <- median(dt_rip_position, na.rm = TRUE)
-  drift_time_rip_ref_pos <- which.min(abs(drift_time - drift_time_rip_ref))
+  aux2 <- rowSums(aux)
+  rip_pos_ind_ref <- which.max(aux2)
+  rip_pos_dt_ref <- drift_time[rip_pos_ind_ref]
 
   m <- -1
   for (i in  c(0, samples)){
@@ -45,42 +40,32 @@ align_td <- function(dir_in, dir_out,samples) {
     aux_string <- paste0("M", i, ".rds")
     aux_list <- readRDS(file.path(dir_in, aux_string))
     aux <- as.matrix(aux_list$data$data_df)
+    aux2 <- rowSums(aux)
 
+    if (i == 0){
+      drift_time <- aux_list$data$drift_time
+      rip_pos_ind_ref <- which.max(aux2)
+      rip_pos_dt_ref <- drift_time[rip_pos_ind_ref]
+    } else {
+      rip_pos_ind <- which.max(aux2)
+      rip_pos_dt <- drift_time[rip_pos_ind]
+      Kcorr <- rip_pos_dt_ref/rip_pos_dt
 
-    # Compute the total ion spectrum
-    aux_2 <- rowSums(aux)
-    peaks_info <- findpeaks(aux_2)
-
-    # look for rip_position of total ion spectrum
-    rip_pos_ind_sum <- which.max(peaks_info[ , 1])
-    rip_pos_sum <- peaks_info[rip_pos_ind_sum, 2]
-
-    # Look for the beginning and ending of the RIP (searching the closest minima to it)
-    valleys_info <- findpeaks(-aux_2)
-    valleys_pos_sum <- valleys_info[ , 2]
-    closest_valley_ind_sum <- which.min(abs(valleys_pos_sum - rip_pos_sum))
-
-    # Select the RIP region
-    if(valleys_pos_sum[closest_valley_ind_sum] < rip_pos_sum){
-      rip_bounds <- valleys_pos_sum[c(closest_valley_ind_sum,closest_valley_ind_sum + 1)]
-    } else if (valleys_pos_sum[closest_valley_ind_sum] > rip_pos_sum){
-      if(closest_valley_ind_sum > 1){
-        rip_bounds <- valleys_pos_sum[c(closest_valley_ind_sum - 1,closest_valley_ind_sum)]
+      if(Kcorr == 1){
+        aux_corr <- aux
       } else {
-        rip_bounds <- valleys_pos_sum[c(1, closest_valley_ind_sum)]
+        aux_corr <- 0*aux
+        drift_time_corr <- Kcorr * drift_time
+        for (j in seq_len(ncol(aux))) {
+          aux_corr[,j] <- interp1(drift_time_corr, aux[,j], drift_time, extrap = TRUE)
+        }
+
       }
+      aux_list$data$data_df <- round(aux_corr)
+      saveRDS(aux_list, file = file.path(dir_out, paste0("M", i, ".rds")))
     }
 
-    # RIP position (in indexes) for all spectra in a sample
-    rip_position <- apply(aux[rip_bounds[1]:rip_bounds[2],], MARGIN = 2, which.max) + rip_bounds[1] - 1
-    Kcorr <- drift_time[drift_time_rip_ref_pos]/drift_time[rip_position]
-    aux_corr <- 0*aux
-    for (j in seq_len(ncol(aux))) {
-      drift_time_corr <- Kcorr[j] * drift_time
-      aux_corr[,j] <- interp1(drift_time_corr, aux[,j], drift_time, extrap = TRUE)
-    }
-    aux_list$data$data_df <- round(aux_corr)
-    saveRDS(aux_list, file = file.path(dir_out, paste0("M", i, ".rds")))
+
   }
 }
 
