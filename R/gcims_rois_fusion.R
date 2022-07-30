f10c7bc2831bd67f4cb69949c2461753c60327bf
Sergio Oller <sergioller@gmail.com>
2022-07-30 12:56:12 +0200

    ROI fusion used confusing names:
    - the apex is the location of the maxima
    - the center is the middle point of the ROI
    
    The function was calling "apex" things that were the center of the ROI.
    
    We also check now that the new indices are within range.
    
    The cluster statistics are taken from the clustering output.

--- R/gcims_rois_fusion.R
+++ R/gcims_rois_fusion.R
@@ -1,61 +1,81 @@
 #' ROIs Fusion
-#' @param clustering_object   Objects obtained from the clustering function
-#' containing a list of all the ROIs with tehir corresponding cluster.
-#' @details `clustering_object` calculates de median coordinates of all the ROIs
-#' of each cluster. All the ROis inside a cluster are resized according to their
-#' geometrical center and the size of the median ROI.
-#' @return A Set of S3 objects.
-#' @family Utility functions
+#'
+#' For each ROI in `peak_list_clustered`, we define additional columns with ROI boundaries.
+#' The center of the ROI is not changed, but the limits are taken so the size of the ROI is
+#' the same for all ROIs in the same cluster, using the median size of the ROIs in the cluster.
+#'
+#'
+#' @param peak_list_clustered The peak list clustered data frame from [group_peak_list()]
+#' @param cluster_stats The cluster statistics data frame from [group_peak_list()]
+#' @return The peak list clustered table with added `ref_roi_[dt|rt]_[min|max]_idx` columns and the cluster statistics
 #' @export
 #' @examples
-#' # Example of ROIs FUsion
-#' tt <- gcims_rois_fusion(peak_clustering)
-#' samplestocheck <- tt$peak_list_clustered[which(tt$peak_list_clustered$cluster == 35), ]
+#' peak_list <- data.frame(
+#'   UniqueID = c("P1", "P2", "P3", "P4"),
+#'   SampleID = c("S1", "S1", "S2", "S2"),
+#'   dt_apex_ms = c(7, 10, 7.1, 10.2),
+#'   rt_apex_s = c(30, 250, 33, 247),
+#'   dt_min_ms = c(6.5, 9.4, 6.6, 9.7),
+#'   dt_max_ms = c(7.7, 10.8, 7.6, 11.1),
+#'   rt_min_s = c(27, 246, 30, 245),
+#'   rt_max_s = c(36, 260, 37, 255)
+#' )
+#' clustering <- group_peak_list(
+#'   peaks = peak_list,
+#'   filter_dt_width_criteria = NULL,
+#'   filter_rt_width_criteria = NULL,
+#'   distance_method = "mahalanobis",
+#'   distance_between_peaks_from_same_sample = Inf,
+#'   clustering = list(method = "kmedoids", Nclusters = "max_peaks_sample"),
+#'   verbose = FALSE
+#' )
+#' roi_fusion_out <- gcims_rois_fusion(clustering$peak_list_clustered, clustering$cluster_stats)
+#' samplestocheck <- roi_fusion_out$peak_list_clustered[which(roi_fusion_out$peak_list_clustered$cluster == 2), ]
+#' library(ggplot2)
 #' ggplot() +
-#'  geom_rect(data = samplestocheck, mapping = aes(xmin=dt_min_idx, xmax=dt_max_idx,
-#'  ymin=rt_min_idx, ymax=rt_max_idx), color= as.factor(samplestocheck$SampleID), alpha=0.5)
+#'  geom_rect(
+#'    data = samplestocheck,
+#'    mapping = aes(
+#'      xmin=dt_min_idx,
+#'      xmax=dt_max_idx,
+#'      ymin=rt_min_idx,
+#'      ymax=rt_max_idx,
+#'      color= SampleID
+#'    ),
+#'    alpha=0.5
+#' )
 #' invisible(file.remove(files))
-gcims_rois_fusion <- function(clustering_object){
-  clusters_infor <- clustering_object$peak_list_clustered
-  clusters_infor$ref_roi_dt_min_idx <- rep(0, dim(clusters_infor)[1])
-  clusters_infor$ref_roi_dt_max_idx <- rep(0, dim(clusters_infor)[1])
-  clusters_infor$ref_roi_rt_min_idx <- rep(0, dim(clusters_infor)[1])
-  clusters_infor$ref_roi_rt_max_idx <- rep(0, dim(clusters_infor)[1])
-  clusters_infor$ref_roi_dt_apex <- rep(0, dim(clusters_infor)[1])
-  clusters_infor$ref_roi_rt_apex <- rep(0, dim(clusters_infor)[1])
-  num_clusts <- unique(clusters_infor$cluster)
-  num_samps <- unique(clusters_infor$SampleID)
-  for(i in num_clusts){
-    # 4 corrdinates
-    ROIs2Fusion <- which(clusters_infor$cluster == i)
-    median_dtmin_clust <- floor(stats::median(clusters_infor$dt_min_idx[ROIs2Fusion]))
-    median_dtmax_clust <- ceiling(stats::median(clusters_infor$dt_max_idx[ROIs2Fusion]))
-    median_rtmin_clust <- floor(stats::median(clusters_infor$rt_min_idx[ROIs2Fusion]))
-    median_rtmax_clust <- ceiling(stats::median(clusters_infor$rt_max_idx[ROIs2Fusion]))
+gcims_rois_fusion <- function(peak_list_clustered, cluster_stats) {
+  # FIXME: Provide these columns in physical units as well.
+  # We will do this afterwards, when we have a dataset object that has
+  # this information (to avoid reading the samples just to get their dtime and rtime)
+  peak_list_clustered$ref_roi_dt_min_idx <- 0
+  peak_list_clustered$ref_roi_dt_max_idx <- 0
+  peak_list_clustered$ref_roi_rt_min_idx <- 0
+  peak_list_clustered$ref_roi_rt_max_idx <- 0
 
-    medianROI <- c(median_dtmin_clust, median_dtmax_clust, median_rtmin_clust, median_rtmax_clust)
+  # Largest valid index. It should come from the sample/dataset, but I'm not going
+  # to load all the samples just for that. We'll fix this later FIXME
+  dt_allmax_idx <- max(c(peak_list_clustered$dt_max_idx, cluster_stats$dt_max_idx))
+  rt_allmax_idx <- max(c(peak_list_clustered$rt_max_idx, cluster_stats$rt_max_idx))
 
-    # Geometric Center
-    median_dtapex_clust <- floor(medianROI[1] + (medianROI[2] - medianROI[1])/2)
-    median_rtapex_clust <- floor(medianROI[3] + (medianROI[4] - medianROI[3])/2)
+  for (i in seq_len(nrow(peak_list_clustered))) {
+    roi_prop <- as.list(peak_list_clustered[i,])
+    cluster_id <- roi_prop$cluster
+    cluster_prop <- as.list(cluster_stats[cluster_stats$cluster == cluster_id,])
 
-    clusters_infor$ref_roi_dt_min_idx[ROIs2Fusion] <- median_dtmin_clust
-    clusters_infor$ref_roi_dt_max_idx[ROIs2Fusion] <- median_dtmax_clust
-    clusters_infor$ref_roi_rt_min_idx[ROIs2Fusion] <- median_rtmin_clust
-    clusters_infor$ref_roi_rt_max_idx[ROIs2Fusion] <- median_rtmax_clust
-    clusters_infor$ref_roi_dt_apex[ROIs2Fusion] <- median_dtapex_clust
-    clusters_infor$ref_roi_rt_apex[ROIs2Fusion] <- median_rtapex_clust
+    dt_cluster_half_length <- ceiling((cluster_prop$dt_max_idx - cluster_prop$dt_min_idx)/2)
+    rt_cluster_half_length <- ceiling((cluster_prop$rt_max_idx - cluster_prop$rt_min_idx)/2)
 
-    for (j in ROIs2Fusion){
-      ROI <- c(clusters_infor$dt_min_idx[j], clusters_infor$dt_max_idx[j],
-               clusters_infor$rt_min_idx[j], clusters_infor$rt_max_idx[j])
-      ROI_dtapex_clust <- floor(ROI[1] + (ROI[2] - ROI[1])/2)
-      ROI_rtapex_clust <- floor(ROI[3] + (ROI[4] - ROI[3])/2)
-      clusters_infor$dt_min_idx[j] <- ROI_dtapex_clust - (median_dtapex_clust - median_dtmin_clust)
-      clusters_infor$dt_max_idx[j] <- ROI_dtapex_clust + (median_dtmax_clust - median_dtapex_clust)
-      clusters_infor$rt_min_idx[j] <- ROI_rtapex_clust - (median_rtapex_clust - median_rtmin_clust)
-      clusters_infor$rt_max_idx[j] <- ROI_rtapex_clust + (median_rtmax_clust - median_rtapex_clust)
-    }
+    dt_roi_center_idx <- floor((roi_prop$dt_max_idx + roi_prop$dt_min_idx)/2)
+    rt_roi_center_idx <- floor((roi_prop$rt_max_idx + roi_prop$rt_min_idx)/2)
+
+    peak_list_clustered$ref_roi_dt_min_idx[i] <- max(1L,            dt_roi_center_idx - dt_cluster_half_length)
+    peak_list_clustered$ref_roi_dt_max_idx[i] <- min(dt_allmax_idx, dt_roi_center_idx + dt_cluster_half_length)
+    peak_list_clustered$ref_roi_rt_min_idx[i] <- max(1L,            rt_roi_center_idx - rt_cluster_half_length)
+    peak_list_clustered$ref_roi_rt_max_idx[i] <- min(rt_allmax_idx, rt_roi_center_idx + rt_cluster_half_length)
+
+  }
 
 
     # thrOverlap <- 0.8
@@ -71,8 +91,9 @@ gcims_rois_fusion <- function(clustering_object){
     #     }
     # }
 
-  }
-  clustering_object$peak_list_clustered <- clusters_infor
-  return(clustering_object)
+  list(
+    peak_list_clustered = peak_list_clustered,
+    cluster_stats = cluster_stats
+  )
 }
 
