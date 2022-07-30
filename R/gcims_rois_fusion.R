b60e56f6290318bfa1806518be18d8b38feb8db5
Sergio Oller <sergioller@gmail.com>
2022-07-30 13:14:03 +0200

    Fix example

--- R/gcims_rois_fusion.R
+++ R/gcims_rois_fusion.R
@@ -13,12 +13,22 @@
 #' peak_list <- data.frame(
 #'   UniqueID = c("P1", "P2", "P3", "P4"),
 #'   SampleID = c("S1", "S1", "S2", "S2"),
+#'   dt_apex_idx = c(140, 340, 180, 380),
 #'   dt_apex_ms = c(7, 10, 7.1, 10.2),
+#'   rt_apex_idx = c(30, 250, 33, 247),
 #'   rt_apex_s = c(30, 250, 33, 247),
+#'   dt_min_idx = c(100, 300, 120, 320),
 #'   dt_min_ms = c(6.5, 9.4, 6.6, 9.7),
+#'   dt_max_idx = c(200, 400, 250, 440),
 #'   dt_max_ms = c(7.7, 10.8, 7.6, 11.1),
+#'   rt_min_idx = c(27, 246, 30, 245),
 #'   rt_min_s = c(27, 246, 30, 245),
-#'   rt_max_s = c(36, 260, 37, 255)
+#'   rt_max_idx = c(36, 260, 37, 255),
+#'   rt_max_s = c(36, 260, 37, 255),
+#'   dt_cm_idx = c(140, 340, 180, 380),
+#'   dt_cm_ms = c(7, 10, 7.1, 10.2),
+#'   rt_cm_idx = c(30, 250, 33, 247),
+#'   rt_cm_s = c(30, 250, 33, 247)
 #' )
 #' clustering <- group_peak_list(
 #'   peaks = peak_list,
@@ -30,7 +40,7 @@
 #'   verbose = FALSE
 #' )
 #' roi_fusion_out <- gcims_rois_fusion(clustering$peak_list_clustered, clustering$cluster_stats)
-#' samplestocheck <- roi_fusion_out$peak_list_clustered[which(roi_fusion_out$peak_list_clustered$cluster == 2), ]
+#' samplestocheck <- roi_fusion_out$peak_list_clustered[which(roi_fusion_out$peak_list_clustered$cluster == "Cluster02"), ]
 #' library(ggplot2)
 #' ggplot() +
 #'  geom_rect(
