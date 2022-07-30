4ef32b9d88a30112e11b70d23efe03b92c795d93
Sergio Oller <sergioller@gmail.com>
2022-07-30 07:45:46 +0200

    Test dtime()

---
+++ tests/testthat/test-dtime-GCIMSSample_S3.R
@@ -0,0 +1,9 @@
+test_that("dtime works on GCIMSSampleS3", {
+  obj <- new_GCIMSSampleS3(
+    drift_time = 1:3,
+    retention_time = 1:5,
+    intensity = matrix(1:15, nrow = 3, ncol = 5),
+    metadata = list(Name = "Sample1")
+  )
+  all(dtime(obj) == 1:3)
+})
