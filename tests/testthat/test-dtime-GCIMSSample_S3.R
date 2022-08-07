6876862b094dbd0874d0f6b9c705984cdfa31197
Sergio Oller <sergioller@gmail.com>
2022-08-07 15:27:44 +0200

    Remove test from unused object

--- tests/testthat/test-dtime-GCIMSSample_S3.R
+++
@@ -1,9 +0,0 @@
-test_that("dtime works on GCIMSSampleS3", {
-  obj <- new_GCIMSSampleS3(
-    drift_time = 1:3,
-    retention_time = 1:5,
-    intensity = matrix(1:15, nrow = 3, ncol = 5),
-    metadata = list(Name = "Sample1")
-  )
-  expect_true(all(dtime(obj) == 1:3))
-})
