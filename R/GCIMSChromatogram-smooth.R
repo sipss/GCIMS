32109e8a20c10e2f1b890a2d8839474de0ba4420
Sergio Oller <sergioller@gmail.com>
2022-08-25 10:40:37 +0200

    Move smooth GCIMSChromatogram to its own file

---
+++ R/GCIMSChromatogram-smooth.R
@@ -0,0 +1,18 @@
+#' Smoothing a GCIMS chromatogram using a Savitzky-Golay filter
+#' @param x A [GCIMSChromatogram] object
+#' @param rt_length_s The length of the filter in retention time (in s)
+#' @param rt_order The order of the filter in retention time
+#' @return The modified [GCIMSChromatogram]
+#' @importMethodsFrom ProtGenerics smooth
+#' @export
+methods::setMethod(
+  "smooth", "GCIMSChromatogram",
+  function(x, rt_length_s = 3, rt_order = 2L) {
+    rt <- rtime(x)
+    rt_length_pts <- units_to_points(rt_length_s, rt[2] - rt[1], must_odd = TRUE)
+    if (rt_length_pts >= 1L) {
+      x@intensity <- signal::sgolayfilt(x@intensity, n = rt_length_pts, p = rt_order)
+    }
+    x
+  }
+)
