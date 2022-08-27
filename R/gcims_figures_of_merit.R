c4333ef575b644725537072b815b6d06f26221d8
Sergio Oller <sergioller@gmail.com>
2022-08-27 09:16:59 +0200

    Refactor find_regions_rip_saturated

--- R/gcims_figures_of_merit.R
+++ R/gcims_figures_of_merit.R
@@ -66,26 +66,7 @@ gcims_figures_of_merit <- function(
     peak_list_rows <- which(peak_list$SampleID == sample_name)
 
     # 2. Find the retention time regions where the RIP is saturated:
-    find_regions_rip_saturated <- function(aux) {
-      the_rip <- find_rip(aux)
-      # Search saturation regions
-      rip_region <- aux[the_rip$dt_idx_start:the_rip$dt_idx_end, , drop = FALSE]
-      rip_chrom <- rowSums(rip_region) / nrow(rip_region)
-      rt_rip_saturated_indices <- which(rip_chrom <= rip_saturation_threshold * max(rip_chrom))
-      if (length(rt_rip_saturated_indices) == 0L) {
-        rt_saturated_regions <- matrix(nrow = 0L, ncol = 2L)
-      } else {
-        saturation_list <- split(rt_rip_saturated_indices, cumsum(c(1, diff(rt_rip_saturated_indices)) != 1))
-        rt_saturated_regions <- matrix(0, nrow = length(saturation_list), ncol = 2)
-        for (k in seq_along(saturation_list)) {
-          rt_saturated_regions[k, 1L] <- min(saturation_list[[k]])
-          rt_saturated_regions[k, 2L] <- max(saturation_list[[k]])
-        }
-      }
-      colnames(rt_saturated_regions) <- c("begin", "end")
-      rt_saturated_regions
-    }
-    rt_saturated_regions <- find_regions_rip_saturated(aux)
+    rt_saturated_regions <- find_regions_rip_saturated(aux, rip_saturation_threshold)
 
     for (peak_list_row in peak_list_rows) {
       roi_prop <- as.list(peak_list[peak_list_row,])
@@ -140,6 +121,33 @@ gcims_figures_of_merit <- function(
   return(peak_list)
 }
 
+
+find_regions_rip_saturated <- function(aux, rip_saturation_threshold, retention_time = NULL) {
+  the_rip <- find_rip(aux)
+  # Search saturation regions
+  rip_region <- aux[the_rip$dt_idx_start:the_rip$dt_idx_end, , drop = FALSE]
+  rip_chrom <- rowSums(rip_region) / nrow(rip_region)
+  rt_rip_saturated_indices <- which(rip_chrom <= rip_saturation_threshold * max(rip_chrom))
+  if (length(rt_rip_saturated_indices) == 0L) {
+    rt_saturated_regions <- matrix(nrow = 0L, ncol = 2L)
+  } else {
+    saturation_list <- split(rt_rip_saturated_indices, cumsum(c(1, diff(rt_rip_saturated_indices)) != 1))
+    rt_saturated_regions <- matrix(0, nrow = length(saturation_list), ncol = 2)
+    for (k in seq_along(saturation_list)) {
+      rt_saturated_regions[k, 1L] <- min(saturation_list[[k]])
+      rt_saturated_regions[k, 2L] <- max(saturation_list[[k]])
+    }
+  }
+  colnames(rt_saturated_regions) <- c("begin", "end")
+  if (!is.null(retention_time)) {
+    rt_saturated_regions_s <- matrix(0.0, nrow = length(saturation_list), ncol = 2L)
+    rt_saturated_regions_s[,1L] <- retention_time[rt_saturated_regions[,1L]]
+    rt_saturated_regions_s[,2L] <- retention_time[rt_saturated_regions[,2L]]
+    return(rt_saturated_regions_s)
+  } else {
+    return(rt_saturated_regions)
+  }
+}
 
 #----------------------#
 #   compute_integral2  #
