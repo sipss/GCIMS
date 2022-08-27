1179abce45436f5be959dede7453e3ec6eddaea3
Sergio Oller <sergioller@gmail.com>
2022-08-27 09:43:11 +0200

    integrate peaks

--- R/GCIMSDataset-integratePeaks.R
+++ R/GCIMSDataset-integratePeaks.R
@@ -1,8 +1,8 @@
 #' Title
 #'
 #' @param object The [GCIMSDataset] object
-#' @param peak_list
-#' @param integration_size_method
+#' @param peak_list A data frame
+#' @param integration_size_method Either fixed_size or free_size
 #' @param rip_saturation_threshold
 #'
 #' @return
@@ -11,11 +11,13 @@
 #' @examples
 integratePeaks <- function(object, peak_list, integration_size_method = c("fixed_size", "free_size"), rip_saturation_threshold = 0.1) {
   integration_size_method <- match.arg(integration_size_method)
-  GCIMS:::GCIMSDelayedOp(
+  delayed_op <- GCIMS:::GCIMSDelayedOp(
     name = "integratePeaks",
     fun = integratePeaks,
     params = list(peak_list = peak_list, integration_size_method = integration_size_method, rip_saturation_threshold),
     fun_extract = peaks,
     fun_aggregate = .findPeaks_fun_aggregate
   )
+  object <- appendDelayedOp(object, delayed_op)
+  invisible(object)
 }
