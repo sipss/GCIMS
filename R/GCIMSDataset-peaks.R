d3848584fac0d456afaeebaf7cb2d376668d8f0e
Sergio Oller <sergioller@gmail.com>
2022-08-28 22:25:01 +0200

    Pass some additional options to findPeaks

--- R/GCIMSDataset-peaks.R
+++ R/GCIMSDataset-peaks.R
@@ -8,11 +8,11 @@
 setMethod(
   "findPeaks",
   "GCIMSDataset",
-  function(object, noise_level = 3, verbose = FALSE) {
+  function(object, noise_level = 3, verbose = FALSE, dt_length_ms = 0.14, rt_length_s = 3, iou_overlap_threshold = 0.2) {
     delayed_op <- GCIMSDelayedOp(
       name = "findPeaks",
       fun = findPeaks,
-      params = list(noise_level = noise_level, verbose = verbose),
+      params = list(noise_level = noise_level, verbose = verbose, dt_length_ms = dt_length_ms, rt_length_s = rt_length_s, iou_overlap_threshold = iou_overlap_threshold),
       fun_extract = peaks,
       fun_aggregate = .findPeaks_fun_aggregate
     )
--- R/GCIMSSample-peaks.R
+++ R/GCIMSSample-peaks.R
@@ -8,16 +8,21 @@
 setMethod(
   "findPeaks",
   "GCIMSSample",
-  function(object, noise_level = 3, verbose = FALSE) {
+  function(object, noise_level = 3, verbose = FALSE, dt_length_ms = 0.14, rt_length_s = 3, iou_overlap_threshold = 0.2) {
     dt <- dtime(object)
     rt <- rtime(object)
     int_mat <- intensity(object)
+    dt_length_pts <- units_to_points(dt_length_ms, dt[2] - dt[1], must_odd = TRUE)
+    rt_length_pts <- units_to_points(rt_length_s, rt[2] - rt[1], must_odd = TRUE)
     peak_list <- peak_detection(
       drift_time = dt,
       retention_time = rt,
       int_mat = int_mat,
       noise_level = noise_level,
-      verbose = verbose
+      verbose = verbose,
+      dt_length_pts = dt_length_pts,
+      rt_length_pts = rt_length_pts,
+      iou_overlap_threshold = iou_overlap_threshold
     )
     peaks(object) <- peak_list
     object
