92755bc3d1cbbd006cfd38202ee64d939cf672f9
Anita Masliah <amasliah@ibecbarcelona.eu>
2022-09-07 13:18:49 +0200

    Ensure required packages are installed

--- R/gui.R
+++ R/gui.R
@@ -1,6 +1,12 @@
 #' @export
 launchGCIMSgui <- function() {
+  gui_deps <- c("shiny", "plotly", "bslib", "knitr",
+                "markdown", "BiocParallel", "ggplot2",
+                "GCIMS", "shinyFiles", "shinycssloaders",
+                "prompter", "yaml", "fs")
+  require_pkgs(gui_deps)
   gui_file <- system.file("gui", "gui.R", package = "GCIMS")
   source(gui_file, local = TRUE)
   shiny::shinyApp(ui, server)
 }
+
