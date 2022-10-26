485049f576dbaefecf6c80f518727863dd74af44
Sergio Oller <sergioller@gmail.com>
2022-10-26 21:43:45 +0200

    Refactor as.data.frame for GCIMSSpectrum

--- R/aaa-class-GCIMSSpectrum.R
+++ R/aaa-class-GCIMSSpectrum.R
@@ -61,25 +61,3 @@ methods::setMethod(
 GCIMSSpectrum <- function(...) {
   methods::new("GCIMSSpectrum", ...)
 }
-
-
-#' @inheritParams base::as.data.frame
-#' @export
-as.data.frame.GCIMSSpectrum <- function(x, row.names = NULL, optional = FALSE, ...) {
-  # Why would anyone give row.names or optional is beyond my understanding...
-  # but that's how the generic is defined
-  out <- data.frame(
-    drift_time_ms = dtime(x),
-    intensity = intensity(x)
-  )
-  if (!is.null(x@baseline)) {
-    basel <- baseline(x)
-    out$baseline <- basel
-  }
-  if (!is.null(row.names)) {
-    rownames(out) <- row.names
-  }
-  out
-}
-
-
