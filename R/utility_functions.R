2b6928ac14efc5aafdba4c270a92e751352dd9ce
Sergio Oller <sergioller@gmail.com>
2022-10-27 22:13:03 +0200

    Simplify function to avoid error on available.packages() when repos are not set

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,38 +1,21 @@
 require_pkgs <- function(pkg, msgs = NULL, ...) {
-  have_pkgs <- purrr::map_lgl(pkg, function(p) {requireNamespace(p, quietly = TRUE)})
-  names(have_pkgs) <- pkg
-  if (!all(have_pkgs)) {
-    missing_pkgs <- names(have_pkgs)[!have_pkgs]
-    aval_pkgs <- rownames(utils::available.packages())
-    missing_cran_pkgs <- intersect(missing_pkgs, aval_pkgs)
-    missing_bioc_pkgs <- setdiff(missing_pkgs, missing_cran_pkgs)
-    if (length(missing_bioc_pkgs) > 0) {
-      if (!"BiocManager" %in% rownames(utils::installed.packages())) {
-        missing_cran_pkgs <- c(missing_cran_pkgs, "BiocManager")
-      }
+    have_pkgs <- purrr::map_lgl(pkg, function(p) {
+        requireNamespace(p, quietly = TRUE)
+    })
+    names(have_pkgs) <- pkg
+    if (!all(have_pkgs)) {
+        missing_pkgs <- names(have_pkgs)[!have_pkgs]
+        parent_call <- format(rlang::caller_call())
+        rlang::abort(
+            message = c(
+                glue::glue("{parent_call} requires additional packages. Please install them. You may want to use:", parent_call = parent_call),
+                glue::glue("    BiocManager::install({deparse(missing_pkgs)})", missing_pkgs = missing_pkgs),
+                msgs
+            ),
+            ...
+        )
     }
-    and_string <- character()
-    if (length(missing_cran_pkgs) > 0) {
-      missing_cran_pkgs <- deparse(missing_cran_pkgs)
-      and_string <- ""
-    }
-    if (length(missing_bioc_pkgs) > 0) {
-      missing_bioc_pkgs <- deparse(missing_bioc_pkgs)
-      and_string <- " and"
-    }
-    parent_call <- format(rlang::caller_call())
-    abort(
-      message = c(
-        glue::glue("{parent_call} requires additional packages. Please install them. You may want to use:", parent_call = parent_call),
-        glue::glue("    install.packages({missing_cran_pkgs}){and_string}", missing_cran_pkgs = missing_cran_pkgs, and_string = and_string),
-        glue::glue("    BiocManager::install({missing_bioc_pkgs})", missing_bioc_pkgs = missing_bioc_pkgs),
-        msgs
-      ),
-      ...
-    )
-  }
 }
-
 
 units_to_points <- function(length_phys, step_phys, must_odd = FALSE) {
   length_pts <- round(length_phys/step_phys)
