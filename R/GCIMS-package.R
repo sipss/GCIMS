94f9f996d30575eecb4f8f7c1e6edd024998140c
Sergio Oller <sergioller@gmail.com>
2023-02-23 08:45:43 +0100

    Import cli functions

--- R/GCIMS-package.R
+++ R/GCIMS-package.R
@@ -4,6 +4,9 @@
 #' @importFrom rlang inform
 #' @importFrom rlang warn
 #' @importFrom rlang abort
+#' @importFrom cli cli_abort
+#' @importFrom cli cli_warn
+#' @importFrom cli cli_inform
 #' @import methods
 #' @keywords internal
 "_PACKAGE"
