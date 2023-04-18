b671492d0284eea4e3bdd45c038474c424febf81
Sergio Oller <sergioller@gmail.com>
2023-04-18 14:53:59 +0200

    sample_name_or_number_to_both now accepts a logical vector as well

--- R/utils-getSample.R
+++ R/utils-getSample.R
@@ -5,6 +5,8 @@ sample_name_or_number_to_both <- function(sample, sample_names) {
       cli_abort("All samples should be between 1 and {length(sample_names)}")
     }
     sample_id <- sample_names[sample_num]
+    logic <- rep(FALSE, length(sample_names))
+    logic[sample_num] <- TRUE
   } else if (is.character(sample)) {
     sample_id <- sample
     sample_num <- match(sample, sample_names)
@@ -12,11 +14,19 @@ sample_name_or_number_to_both <- function(sample, sample_names) {
     if (length(missing_sample_names) > 0) {
       cli_abort("Missing sample names: {paste0(missing_sample_names, collapse = ', ')}")
     }
+    logic <- rep(FALSE, length(sample_names))
+    logic[sample_num] <- TRUE
+  } else if (is.logical(sample)) {
+    if (length(sample) != length(sample_names)) {
+      cli_abort("When {.var sample} is a logical vector it should be of length {length(sample_names)} (the number of samples)")
+    }
+    logic <- sample
+    logic[is.na(logic)] <- FALSE
+    sample_id <- sample_names[logic]
+    sample_num <- seq_along(sample_names)[logic]
   } else {
-    cli_abort("Sample should be a numeric or a character vector, not {sample}")
+    cli_abort("{.var sample} should be a numeric vector, a character vector or a logical vector, not {class(sample)}")
   }
-  logic <- rep(FALSE, length(sample_names))
-  logic[sample_num] <- TRUE
 
   list(
     idx = sample_num,
