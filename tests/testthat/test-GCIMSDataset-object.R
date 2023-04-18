3c4ef691e25b87a1cb2a66327d6c2d0b4a204fc3
Sergio Oller <sergioller@gmail.com>
2023-04-18 16:53:00 +0200

    Test subset

--- tests/testthat/test-GCIMSDataset-object.R
+++ tests/testthat/test-GCIMSDataset-object.R
@@ -1,9 +1,10 @@
-create_dummy_dataset <- function() {
+create_dummy_dataset <- function(on_ram = TRUE) {
   s1 <- GCIMSSample(1:3, 1:2, matrix(1, nrow = 3, ncol = 2))
   s2 <- GCIMSSample(1:3, 1:2, matrix(2, nrow = 3, ncol = 2))
   d1 <- GCIMSDataset$new_from_list(
     samples = list(s1 = s1, s2 = s2),
-    on_ram = TRUE
+    on_ram = on_ram,
+    scratch_dir = NULL
   )
   list(s1 = s1, s2 = s2, d1 = d1)
 }
@@ -34,9 +35,23 @@ test_that("Get a sample", {
 })
 
 
-test_that("subset works", {
-  a <- create_dummy_dataset()
+test_that("subset on RAM works", {
+  a <- create_dummy_dataset(on_ram = TRUE)
   b <- a$d1$subset(samples = "s2", inplace = FALSE)
   expect_equal(a$d1$sampleNames, c("s1", "s2"))
   expect_equal(b$sampleNames, "s2")
+})
+
+
+test_that("subset on disk works", {
+  d1 <- create_dummy_dataset(on_ram = FALSE)$d1
+  d2 <- d1$subset(samples = "s2", inplace = FALSE, new_scratch_dir = NULL)
+  expect_equal(d1$sampleNames, c("s1", "s2"))
+  expect_equal(d2$sampleNames, "s2")
+  # Different scratch directory:
+  expect_true(dirname(d1$getCurrentDir()) != dirname(d2$getCurrentDir()))
+  # Same step:
+  expect_true(basename(d1$getCurrentDir()) == basename(d2$getCurrentDir()))
+  expect_identical(list.files(d1$getCurrentDir()), c("dataset.rds", "sample_s1.rds", "sample_s2.rds"))
+  expect_identical(list.files(d2$getCurrentDir()), c("dataset.rds", "sample_s2.rds"))
 })
