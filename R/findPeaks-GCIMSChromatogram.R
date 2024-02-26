271bb632cb3100342457c5f5e38d7cc3e49c6340
lalo-caballero <ecaballero@ibecbarcelona.eu>
2024-02-26 14:03:48 +0000

    Add findpeaks for GCIMSSpectrum

--- R/findPeaks-GCIMSChromatogram.R
+++ R/findPeaks-GCIMSChromatogram.R
@@ -20,3 +20,26 @@ setMethod(
     object
   }
 )
+
+#' Peak detection for a GCIMSSpectrum
+#' @param object A [GCIMSSpectrum] object
+#' @inheritDotParams findPeaksImpl1D -x -y
+#' @return The modified [GCIMSSpectrum], with a peak list
+#' @family GCIMSSpectrum
+#' @export
+setMethod(
+  "findPeaks",
+  "GCIMSSpectrum",
+  function(object, ...) {
+    dt <- dtime(object)
+    intens <- intensity(object)
+    peak_list_and_debug_info <- findPeaksImpl1D(
+      x = dt,
+      y = intens,
+      ...
+    )
+    object@peaks_debug_info <- peak_list_and_debug_info$debug_info
+    peaks(object) <- peak_list_and_debug_info$peak_list
+    object
+  }
+)
